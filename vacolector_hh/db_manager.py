cc1b2f396dc10233794a2312dadcddc9530b2aef
Tigran Saatchyan TSA <mr.saatchyan@yandex.com>
2023-07-06 04:11:08 +0400

    [FEATURE] add DB Manager class

---
+++ vacolector_hh/db_manager.py
@@ -0,0 +1,18 @@
+from vacolector_hh.db_engine import DBEngine
+
+
+class DBManager(DBEngine):
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    def get_all_vacancies(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self):
+        pass
